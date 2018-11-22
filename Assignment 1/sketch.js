dd75005dada40b3f43f5a39db68e9f6583f922eb
Natalie <nchan2@uopnet.plymouth.ac.uk>
2018-11-22 12:00:46 +0000

    Update

--- Assignment 1/sketch.js
+++ Assignment 1/sketch.js
@@ -75,4 +75,21 @@ else {
   rotate(frameCount / -50.0); //rotates the shape counter clockwise
   ellipse(252, 144, 72, 72); //creates the ellipse shape
   pop(); //restores the settings used from push
+
+    star(297, 420.5, 30, 70, 5); //star shape
+}
+
+function star(x, y, radius1, radius2, npoints) { //star shape function
+  var angle = TWO_PI / npoints;
+  var halfAngle = angle/2.0;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius2;
+    var sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a+halfAngle) * radius1;
+    sy = y + sin(a+halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
